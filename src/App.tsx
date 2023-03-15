91123dd94e50ac49721fbb13fe6950231c467fc8
joker69216 <hamaraad883@gmail.com>
2023-03-15 17:59:12 +0300

    clean the code / add styles to the headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,14 @@
-// import NavBar from './Components/Navbar/NavBar';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stars, Cloud, Html, Text3D, Sky } from '@react-three/drei';
-// import Background from './Components/Background'
+import NavBar from './Components/Navbar/NavBar';
+
+import Background from './Components/StarsBackground/StarsBackground'
 
 import './App.scss';
 
 function App() {
   return (
-    <div
-      style={{
-        height: '500px',
-        width: '100%',
-      }}
-    >
-      <Canvas
-        camera={{
-          position: [0, 10, 20],
-        }}
-        style={
-          {
-            width: '100%'
-          }
-        }
-      >
-        <group>
-          <Html
-          
-            position={[0, 0, 0]}
-            style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-              position: 'relative'
-            }}
-          >
-            <h1
-              style={{
-                fontSize: '3rem',
-                fontWeight: 'bold',
-                color: 'white',
-                width: '100%',
-              }}
-            >
-              Mhamad Raad
-            </h1>
-            <h2>Front-end Developer</h2>
-          </Html>
-        </group>
-        <OrbitControls enableZoom={false} />
-        <Stars 
-          radius={100}
-          depth={100}
-          count={5000} 
-          factor={4} 
-          saturation={1} 
-          fade 
-          speed={1.25}
-         />
-      </Canvas>
-
-      <Canvas  camera={{
-          position: [0, 10, 20],
-        }}>
-      </Canvas>
+    <div className="app-page">
+      <NavBar />
+      <Background />
     </div>
   );
 }
---
+++ src/Components/StarsBackground/StarsBackground.jsx
@@ -0,0 +1,35 @@
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+
+import './StarsBackground.scss'
+
+export default function Background() {
+  return (
+    <div className="stars-bg">
+      <Canvas
+        camera={{
+        position: [0, 1, 0],
+        }}
+            >
+        <OrbitControls enableZoom={false} />
+        <Stars 
+          radius={100}
+          depth={100}
+          count={5000} 
+          factor={4} 
+          saturation={1} 
+          fade 
+          speed={1.25}
+        />
+      </Canvas>
+      <div className="name-position-bg">
+        <h1 className="name-position-bg__name" >
+          Mhamad Raad
+        </h1>
+        <h2 className="name-position-bg__position">
+          Front-end Developer
+        </h2>
+      </div>
+    </div>
+  );
+}
