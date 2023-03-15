0441993c8cd5c457ebceed028a6d1b21d37f3fdc
joker69216 <hamaraad883@gmail.com>
2023-03-15 18:06:21 +0300

    fix formatter settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import NavBar from './Components/Navbar/NavBar';
 
-import Background from './Components/StarsBackground/StarsBackground'
+import Background from './Components/StarsBackground/StarsBackground';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="app-page">
+    <div className='app-page'>
       <NavBar />
       <Background />
     </div>
