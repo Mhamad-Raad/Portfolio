56bab48f16e2191c6cfefe149bd7664a267fa638
joker69216 <hamaraad883@gmail.com>
2023-03-22 15:39:41 +0300

    change the gltf to jsx to make it cast shadow

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Plane } from '@react-three/drei';
 
 import SamuraiModel from '../CanvaObjects/SamuraiModel'
 import './AboutMe.scss'
@@ -9,19 +9,32 @@ interface AboutMeProps {}
 
 const AboutMe: FC<AboutMeProps> = () => {
   return (
-    <section className="about-me">
-      <div className="about-me__description">
-      </div>
-      <div className="about-me__samurai">
+    <section className='about-me'>
+      <div className='about-me__description'></div>
+      <div className='about-me__samurai'>
         <Canvas shadows>
           <OrbitControls />
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
+          {/* <pointLight position={[10, 10, 10]} castShadow />
+           */}
+          <directionalLight
+            visible
+            intensity={1}
+            position={[0, 10, 5]}
+            castShadow // highlight-line
+          />
           <SamuraiModel />
+          <Plane
+            receiveShadow // highlight-line
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -5, 0]}
+            args={[1000, 1000]}
+          >
+            <meshStandardMaterial attach='material' color='white' />
+          </Plane>
         </Canvas>
       </div>
     </section>
-  )
+  );
 }
 
 export default AboutMe;
--- src/Components/CanvaObjects/SamuraiModel.jsx
+++ src/Components/CanvaObjects/SamuraiModel.jsx
@@ -1,19 +1,18 @@
 import { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import Samurai from './Scene';
 
 const SamuraiModel = () => {
   const group = useRef();
-  const { scene } = useGLTF('./models/samurai.glb');
+
 
   useFrame(() => {
     // group.current.rotation.y += 0.01;
   });
 
   return (
-    <group ref={group} position={[0, -2, 0]} scale={2}>
-      <primitive object={scene} castShadow/>
-    </group>
+    <Samurai />
   );
 };
 
