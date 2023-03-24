c58ef34c79a0659dfd73ce45750560b1271b1255
joker69216 <hamaraad883@gmail.com>
2023-03-25 02:22:05 +0300

    add framer motion to the website

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Plane, Cloud } from '@react-three/drei';
+import { useInView } from 'framer-motion';
 
 import SamuraiModel from '../CanvaObjects/SamuraiModel';
 import './AboutMe.scss';
@@ -8,24 +9,44 @@ import './AboutMe.scss';
 interface AboutMeProps {}
 
 const AboutMe: FC<AboutMeProps> = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <section className='about-me'>
-      <div className='about-me__description'>
+    <section className='about-me' ref={ref}>
+      <div
+        className='about-me__description'
+        style={{
+          transform: isInView ? 'none' : 'translateX(-200px)',
+          opacity: isInView ? 1 : 0,
+          transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+        }}
+      >
         <h2 className='about-me__description__title'>
           Hi There{' '}
-          <span className='about-me__description__title__wave'>👋</span>
-          , I am Mhamad
+          <span className='about-me__description__title__wave'>👋</span>, I am
+          Mhamad
         </h2>
         <p className='about-me__description__me'>
-          I am a Software Engineer Graduate, that is very passionate about learning new technologies,
-          making new connections, and make a career in front end development, of course in this field only tech skills wont be enough,
-          thoughout my experience and journey I have also improved my time management, and communications, but also I have recieved
-          a lot of feedback from my colleagues and friends, such as I am highly adaptive, dedicated, and hard working, like
-          a Samurai which is a great honor toward both work and my surroundings,
-          interested in learning more?
+          I am a Software Engineer Graduate, that is very passionate about
+          learning new technologies, making new connections, and make a career
+          in front end development, of course in this field only tech skills
+          wont be enough, thoughout my experience and journey I have also
+          improved my time management, and communications, but also I have
+          recieved a lot of feedback from my colleagues and friends, such as I
+          am highly adaptive, dedicated, and hard working, like a Samurai which
+          is a great honor toward both work and my surroundings, interested in
+          learning more?
         </p>
       </div>
-      <div className='about-me__samurai'>
+      <div
+        className='about-me__samurai'
+        style={{
+          transform: isInView ? 'none' : 'translateX(200px)',
+          opacity: isInView ? 1 : 0,
+          transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+        }}
+      >
         <Canvas
           shadows
           camera={{ fov: 100, near: 0.1, far: 1000, position: [1.5, 1, 1.5] }}
@@ -38,7 +59,7 @@ const AboutMe: FC<AboutMeProps> = () => {
             visible
             intensity={1}
             position={[0, 10, 5]}
-            castShadow // highlight-line
+            castShadow
           />
           <ambientLight intensity={0.3} />
           <SamuraiModel />
