4f19e9ed32731e730fad389f601da59059d32baa
joker69216 <hamaraad883@gmail.com>
2023-03-24 23:51:21 +0300

    update the about me title

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -11,14 +11,25 @@ const AboutMe: FC<AboutMeProps> = () => {
   return (
     <section className='about-me'>
       <div className='about-me__description'>
-        <h2 className='about-me__description__title'>About Me</h2>
+        <h2 className='about-me__description__title'>
+          Hi There <span className='about-me__description__title__wave'>👋</span>,
+          I am Mhamad
+        </h2>
+        <h4 className='about-me__description__subheader'></h4>
+        <p>
+          I am a full stack developer with a passion for learning new
+          technologies. I have a background in the hospitality industry and
+          customer service. I am a self-taught developer who is always looking
+          to improve my skills and learn new things. I am currently looking for
+          a full time position as a developer.
+        </p>
       </div>
       <div className='about-me__samurai'>
         <Canvas
           shadows
           camera={{ fov: 100, near: 0.1, far: 1000, position: [1.5, 1, 1.5] }}
           style={{
-            borderRadius: '10px'
+            borderRadius: '10px',
           }}
         >
           <OrbitControls />
@@ -38,7 +49,11 @@ const AboutMe: FC<AboutMeProps> = () => {
           >
             <meshStandardMaterial attach='material' color='grey' />
           </Plane>
-          <Cloud opacity={0.75} position={[-3, 1, -5]} scale={[0.5, 0.5, 0.5]} />
+          <Cloud
+            opacity={0.75}
+            position={[-3, 1, -5]}
+            scale={[0.5, 0.5, 0.5]}
+          />
           <Cloud
             opacity={0.75}
             position={[-6, 0, -1]}
