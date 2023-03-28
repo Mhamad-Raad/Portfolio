ad6a8908ab34bb22aeb350d33b1a7415fb04d510
joker69216 <hamaraad883@gmail.com>
2023-03-28 17:43:35 +0300

    add header to the experience section

--- src/Components/WorkExperience/WorkExperience.tsx
+++ src/Components/WorkExperience/WorkExperience.tsx
@@ -9,9 +9,11 @@ import './WorkExperience.scss';
 export default function WorkExperience() {
   return (
     <section className='work-experience column'>
+      <h2 className='work-experience__title'>Work Experience</h2>
+      <h4 className='work-experience__subtitle'>WHAT I HAVE DONE SO FAR</h4>
       <VerticalTimeline>
         <VerticalTimelineElement
-          className='vertical-timeline-element--work'
+          // className='vertical-timeline-element--work'
           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date='2011 - present'
@@ -26,9 +28,10 @@ export default function WorkExperience() {
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className='vertical-timeline-element--work'
+          // className='vertical-timeline-element--work'
           date='2010 - 2011'
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           // icon={<WorkIcon />}
         >
           <h3 className='vertical-timeline-element-title'>Art Director</h3>
@@ -41,7 +44,7 @@ export default function WorkExperience() {
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className='vertical-timeline-element--work'
+          // className='vertical-timeline-element--work'
           date='2008 - 2010'
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           // icon={<WorkIcon />}
@@ -53,7 +56,7 @@ export default function WorkExperience() {
           <p>User Experience, Visual Design</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className='vertical-timeline-element--work'
+          // className='vertical-timeline-element--work'
           date='2006 - 2008'
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           // icon={<WorkIcon />}
@@ -65,7 +68,7 @@ export default function WorkExperience() {
           <p>User Experience, Visual Design</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
+          // className='vertical-timeline-element--education'
           date='April 2013'
           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
           // icon={<SchoolIcon />}
@@ -77,7 +80,7 @@ export default function WorkExperience() {
           <p>Strategy, Social Media</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
+          // className='vertical-timeline-element--education'
           date='November 2012'
           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
           // icon={<SchoolIcon />}
@@ -89,7 +92,7 @@ export default function WorkExperience() {
           <p>Creative Direction, User Experience, Visual Design</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
+          // className='vertical-timeline-element--education'
           date='2002 - 2006'
           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
           // icon={<SchoolIcon />}
