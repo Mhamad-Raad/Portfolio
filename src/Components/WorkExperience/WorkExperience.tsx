a02556cf588bb63c7dc7e189327b55a26d38bf34
joker69216 <hamaraad883@gmail.com>
2023-03-28 20:50:39 +0300

    add needed images and new jobs

--- src/Components/WorkExperience/WorkExperience.tsx
+++ src/Components/WorkExperience/WorkExperience.tsx
@@ -7,6 +7,8 @@ import 'react-vertical-timeline-component/style.min.css';
 import './WorkExperience.scss';
 
 import LezanImg from '../../assets/Lezan.jpg';
+import MicroverseImg from '../../assets/Microverse.jpg';
+import GatewayImg from '../../assets/Gateway.png';
 
 export default function WorkExperience() {
   return (
@@ -15,34 +17,74 @@ export default function WorkExperience() {
       <h4 className='work-experience__subtitle'>WHAT I HAVE DONE SO FAR</h4>
       <VerticalTimeline lineColor='#8ab6f9'>
         <VerticalTimelineElement
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentStyle={{ background: '#7D7D7D', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date='Oct-2022 - Apr-2023'
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<img src={LezanImg} alt='Lezan Logo' className='work-experience-logo' />}
+          date='OCT-2022 - APR-2023'
+          dateClassName='work-experience__date'
+          iconClassName='work-experience__icon'
+          icon={
+            <img
+              src={GatewayImg}
+              alt='Lezan Logo'
+              className='work-experience__logo'
+              title='Gateway for ICT Solutions'
+            />
+          }
         >
-          <h3 className='vertical-timeline-element-title'>
+          <h3 className='vertical-timeline-element-title work-experience__job-title'>
             Front-end Developer
           </h3>
-          <h4 className='vertical-timeline-element-subtitle'>
+          <h4 className='vertical-timeline-element-subtitle work-experience__job-subtitle'>
             Internship, Gateway for ICT Solutions
           </h4>
-          <p>React Developer, Payment Intigration, Agile(Scrum), Team Player</p>
+          <p>
+            React Developer, Payment Intigration, Agile(Scrum), SASS, Team
+            Player, Time Management
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          date='2010 - 2011'
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          date='AUG-2022 - MAR-2023'
+          dateClassName='work-experience__date'
+          iconClassName='work-experience__icon'
           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          // icon={<WorkIcon />}
+          icon={
+            <img
+              src={MicroverseImg}
+              alt='Mircroverse Logo'
+              className='work-experience__logo'
+              title='Microverse'
+            />
+          }
         >
-          <h3 className='vertical-timeline-element-title'>Art Director</h3>
+          <h3 className='vertical-timeline-element-title work-experience__job-title'>
+            Web Development (Mentor)
+          </h3>
           <h4 className='vertical-timeline-element-subtitle'>
-            San Francisco, CA
+            Volunteer, Microverse
           </h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
-          </p>
+          <p>HTML, CSS, Management, Communication, Teamwork</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          date='MAR-2022 - JUL-2022'
+          dateClassName='work-experience__date'
+          iconClassName='work-experience__icon'
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={
+            <img
+              src={LezanImg}
+              alt='Lezan'
+              className='work-experience__logo'
+              title='Lezan'
+            />
+          }
+        >
+          <h3 className='vertical-timeline-element-title work-experience__job-title'>
+            Flutter Developer
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            ApprenticeShip, Lezan
+          </h4>
+          <p>Flutter, Firebase, Team lead, Communication, Teamwork</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </section>
