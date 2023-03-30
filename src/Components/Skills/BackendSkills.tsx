6c05bbae1ce017e5cb118b94d5f069670e2e6119
joker69216 <hamaraad883@gmail.com>
2023-03-30 17:34:16 +0300

    add needed pictures

--- src/Components/Skills/BackendSkills.tsx
+++ src/Components/Skills/BackendSkills.tsx
@@ -4,7 +4,9 @@ export default function BackendSkills() {
   return (
     <div className='category-skills column'>
       <h3 className='category-skills__title'>Back-End</h3>
-      <div className='category-skills__techs'></div>
+      <div className='category-skills__techs'>
+        
+      </div>
     </div>
   );
 }
