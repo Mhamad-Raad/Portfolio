16e9de6c2fd0058b41130e075cb0936ecaf381e9
joker69216 <hamaraad883@gmail.com>
2023-03-30 19:54:51 +0300

    add the extra images

--- src/Components/Skills/ToolsMethodes.tsx
+++ src/Components/Skills/ToolsMethodes.tsx
@@ -5,18 +5,18 @@ import './CategorySkills.scss';
 import AgileImg from '../../assets/ToolsMethodes/Agile.png';
 import CDTImg from '../../assets/ToolsMethodes/CDT.png';
 import GitImg from '../../assets/ToolsMethodes/Git.png';
-// import JiraImg from '../../assets/ToolsMethodes/Jira.png';
+import JiraImg from '../../assets/ToolsMethodes/Jira.png';
 import Github from '../../assets/ToolsMethodes/Github.png';
-// import TrelloImg from '../../assets/ToolsMethodes/Trello.png';
-// import VSCodeImg from '../../assets/ToolsMethodes/VSCode.png';
+import TrelloImg from '../../assets/ToolsMethodes/Trello.png';
+import VSCodeImg from '../../assets/ToolsMethodes/VSCode.png';
 import FirebaseImg from '../../assets/ToolsMethodes/Firebase.png';
-// import FigmaImg from '../../assets/ToolsMethodes/Figma.png';
+import FigmaImg from '../../assets/ToolsMethodes/Figma.png';
 import MFAImg from '../../assets/ToolsMethodes/MFA.png';
 import TDDImg from '../../assets/ToolsMethodes/TDD.png';
 import MongoImg from '../../assets/ToolsMethodes/Mongo.png';
 import NetlifyImg from '../../assets/ToolsMethodes/Netlify.png';
-// import NPMImg from '../../assets/ToolsMethodes/NPM.png'
-// import PostmanImg from '../../assets/ToolsMethodes/Postman.png'
+import NPMImg from '../../assets/ToolsMethodes/NPM.png'
+import PostmanImg from '../../assets/ToolsMethodes/Postman.png'
 import RenderImg from '../../assets/ToolsMethodes/Render.png';
 import VercelImg from '../../assets/ToolsMethodes/Vercel.jpg';
 
