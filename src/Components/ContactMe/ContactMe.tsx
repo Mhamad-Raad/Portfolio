e1ee3dc2f2e4e305c065b428adda2c8e51842044
joker69216 <hamaraad883@gmail.com>
2023-03-31 22:26:39 +0300

    add folder structure for contact me

---
+++ src/Components/ContactMe/ContactMe.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import 'ContactMe.scss';
+
+interface ContactMeInterface {}
+
+const ContactMe: FC<ContactMeInterface> = () => {
+  return (
+    <div>ContactMe</div>
+  )
+}
+
+export default ContactMe;
