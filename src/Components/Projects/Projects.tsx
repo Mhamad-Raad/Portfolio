18bc7384825af5a8172fa8b261a6cf80e3d85131
joker69216 <hamaraad883@gmail.com>
2023-03-31 22:27:47 +0300

    add folder structure for Projects section

---
+++ src/Components/Projects/Projects.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import 'Projects.scss';
+
+interface ProjectsInterface {}
+
+const Projects: FC<ProjectsInterface> = () => {
+  return <div>Projects</div>;
+};
+
+export default Projects;
