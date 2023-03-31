cec54996b5a9689ec8e1323f23177480f3e7cdb8
joker69216 <hamaraad883@gmail.com>
2023-03-31 22:18:26 +0300

    adjust images qualkty

--- src/Components/Skills/ToolsMethodes.tsx
+++ src/Components/Skills/ToolsMethodes.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 
+import CircleSkill from './Skill/CircleSkill';
+
 import './CategorySkills.scss';
 
 import AgileImg from '../../assets/ToolsMethodes/Agile.png';
 import CDTImg from '../../assets/ToolsMethodes/CDT.png';
-import GitImg from '../../assets/ToolsMethodes/Git.png';
+import GitImg from '../../assets/ToolsMethodes/Git.jpg';
 import JiraImg from '../../assets/ToolsMethodes/Jira.png';
-import Github from '../../assets/ToolsMethodes/Github.png';
+import Github from '../../assets/ToolsMethodes/GitHub.png';
 import TrelloImg from '../../assets/ToolsMethodes/Trello.png';
 import VSCodeImg from '../../assets/ToolsMethodes/VSCode.png';
 import FirebaseImg from '../../assets/ToolsMethodes/Firebase.png';
@@ -15,8 +17,9 @@ import MFAImg from '../../assets/ToolsMethodes/MFA.png';
 import TDDImg from '../../assets/ToolsMethodes/TDD.png';
 import MongoImg from '../../assets/ToolsMethodes/Mongo.png';
 import NetlifyImg from '../../assets/ToolsMethodes/Netlify.png';
-import NPMImg from '../../assets/ToolsMethodes/NPM.png'
-import PostmanImg from '../../assets/ToolsMethodes/Postman.png'
+import NPMImg from '../../assets/ToolsMethodes/Npm.png';
+import YarnImg from '../../assets/ToolsMethodes/Yarn.png';
+import PostmanImg from '../../assets/ToolsMethodes/Postman.png';
 import RenderImg from '../../assets/ToolsMethodes/Render.png';
 import VercelImg from '../../assets/ToolsMethodes/Vercel.jpg';
 
@@ -26,7 +29,26 @@ const ToolsMethodes: FC<ToolsMethodesInterface> = () => {
   return (
     <div className='category-skills column'>
       <h3 className='category-skills__title'>Tools & Methodes</h3>
-      <div className='category-skills__techs'></div>
+      <div className='category-skills__techs'>
+        <CircleSkill img={AgileImg} title='Agile' />
+        <CircleSkill img={CDTImg} title='CDT' />
+        <CircleSkill img={GitImg} title='Git' />
+        <CircleSkill img={JiraImg} title='Jira' />
+        <CircleSkill img={Github} title='Github' />
+        <CircleSkill img={TrelloImg} title='Trello' />
+        <CircleSkill img={VSCodeImg} title='VSCode' />
+        <CircleSkill img={FirebaseImg} title='Firebase' />
+        <CircleSkill img={FigmaImg} title='Figma' />
+        <CircleSkill img={MFAImg} title='Mobile First Approach' />
+        <CircleSkill img={TDDImg} title='Test Driven Development' />
+        <CircleSkill img={MongoImg} title='MongoDB' />
+        <CircleSkill img={NetlifyImg} title='Netlify' />
+        <CircleSkill img={NPMImg} title='Node Package Manager' />
+        <CircleSkill img={YarnImg} title='Yarn Package Manager' />
+        <CircleSkill img={PostmanImg} title='Postman' />
+        <CircleSkill img={RenderImg} title='Render' />
+        <CircleSkill img={VercelImg} title='Vercel' />
+      </div>
     </div>
   );
 };
