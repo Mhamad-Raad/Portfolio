658ff3362ee4b5ef549d801ab541da1586ab88d0
joker69216 <hamaraad883@gmail.com>
2023-03-31 22:28:47 +0300

    add folder structure for Recommendations section

---
+++ src/Components/Recommendations/Recommnendations.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import 'Recommentations.scss';
+
+interface RecommentationsInterface {}
+
+const Recommentations: FC<RecommentationsInterface> = () => {
+  return <div>Recommentations</div>;
+};
+
+export default Recommentations;
