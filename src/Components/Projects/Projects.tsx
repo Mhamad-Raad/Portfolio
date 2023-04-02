4245122412acf28359ecc3e6d24bbffbbbb50223
joker69216 <hamaraad883@gmail.com>
2023-04-02 21:29:34 +0300

    add title to projects sections

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -5,7 +5,11 @@ import './Projects.scss';
 interface ProjectsInterface {}
 
 const Projects: FC<ProjectsInterface> = () => {
-  return <section>Projects</section>;
+  return (
+    <section className='projects-section column'>
+      <h2 className='projects-section__title'>Projects</h2>
+    </section>
+  );
 };
 
 export default Projects;
