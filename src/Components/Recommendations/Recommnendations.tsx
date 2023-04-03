8cc181a322d1d3187b74a207c65eb417a7c689ef
joker69216 <hamaraad883@gmail.com>
2023-04-03 18:58:05 +0300

    add structure for the slide show

--- src/Components/Recommendations/Recommnendations.tsx
+++ src/Components/Recommendations/Recommnendations.tsx
@@ -2,12 +2,37 @@ import { FC } from 'react';
 
 import './Recommendations.scss';
 
+import img1 from '../../assets/ToolsMethodes/GitHub.png';
+
 interface RecommentationsInterface {}
 
 const Recommentations: FC<RecommentationsInterface> = () => {
   return (
     <section className='recommendation-section column'>
       <h2 className='recommendation-section__title'>Recommentations</h2>
+      <div className='recommendation-section__slideshow column'>
+        <div className='recommendation-section__slideshow__slide'>
+          <img
+            src={img1}
+            title='img'
+            alt='img'
+            className='recommendation-section__slideshow__slide__image'
+          />
+          <p className='recommendation-section__slideshow__slide__name'>
+            John Doe
+          </p>
+          <p className='recommendation-section__slideshow__slide__position'>
+            CEO, Company
+          </p>
+          <div className='recommendation-section__slideshow__slide__seperator' />
+          <p className='recommendation-section__slideshow__slide__description'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt, nisl eget ultricies tincidunt, nisl elit lacinia nisl, a
+            lacinia nisl lorem non nisl. Sed tincidunt, nisl eget ultricies
+            tincidunt, nisl elit lacinia nisl, a lacinia nisl lorem non nisl.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
