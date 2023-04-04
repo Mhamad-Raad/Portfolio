1737b2fa3836937e22d0dcbf276016734d721e70
joker69216 <hamaraad883@gmail.com>
2023-04-05 00:53:18 +0300

    add left adn right buttons

--- src/Components/Recommendations/Recommnendations.tsx
+++ src/Components/Recommendations/Recommnendations.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
 import Slide from './Slide';
 
 import './Recommendations.scss';
@@ -10,10 +12,23 @@ const Recommentations: FC<RecommentationsInterface> = () => {
   return (
     <section className='recommendation-section column'>
       <h2 className='recommendation-section__title'>Recommentations</h2>
+
       <div className='recommendation-section__slideshow row'>
-          <Slide />
-          <Slide />
-          <Slide />
+        <button
+          type='button'
+          className='recommendation-section__slideshow__left-scroll-button'
+        >
+          <FaAngleLeft />
+        </button>
+        <Slide />
+        <Slide />
+        <Slide />
+        <button
+          type='button'
+          className='recommendation-section__slideshow__right-scroll-button'
+        >
+          <FaAngleRight />
+        </button>
       </div>
     </section>
   );
