de0e0d8d2dcc06cfb5c67ee6ca2be811210bc5e6
joker69216 <hamaraad883@gmail.com>
2023-04-07 23:22:11 +0300

    add title to contact me section

--- src/Components/ContactMe/ContactMe.tsx
+++ src/Components/ContactMe/ContactMe.tsx
@@ -5,7 +5,11 @@ import './ContactMe.scss';
 interface ContactMeInterface {}
 
 const ContactMe: FC<ContactMeInterface> = () => {
-  return <section className='contact-me'>ContactMe</section>;
+  return (
+    <section className='contact-me column'>
+      <h2 className='contact-me__title'>Contact Me</h2>
+    </section>
+  );
 };
 
 export default ContactMe;
