b6401173f8c3a7b70d04d2a35ca29aceaff014d3
joker69216 <hamaraad883@gmail.com>
2023-04-08 01:54:25 +0300

    add structure for the contact me form

--- src/Components/ContactMe/ContactMe.tsx
+++ src/Components/ContactMe/ContactMe.tsx
@@ -10,7 +10,16 @@ const ContactMe: FC<ContactMeInterface> = () => {
   return (
     <section className='contact-me column'>
       <h2 className='contact-me__title'>Contact Me</h2>
-      <Blob />
+      <div className='contact-me__content row'>
+        <form className='contact-me__content__form column'>
+          <h3 className='contact-me__content__form__title'> Let's Connect</h3>
+          <input type='text' placeholder='Name' />
+          <input type='email' placeholder='Email' />
+          <textarea placeholder='Message' />
+          <button type='submit'>Send</button>
+        </form>
+        <Blob />
+      </div>
     </section>
   );
 };
