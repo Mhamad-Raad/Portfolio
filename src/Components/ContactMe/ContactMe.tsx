bc37fcbedd062e59a844c262f8aaff7ca56a9331
joker69216 <hamaraad883@gmail.com>
2023-04-08 20:45:31 +0300

    add functionality to the form

--- src/Components/ContactMe/ContactMe.tsx
+++ src/Components/ContactMe/ContactMe.tsx
@@ -1,4 +1,5 @@
-import { FC, FormEvent } from 'react';
+import { FC, FormEvent, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import Blob from '../CanvaObjects/Blob/Blob';
 
@@ -7,19 +8,50 @@ import './ContactMe.scss';
 interface ContactMeInterface {}
 
 const ContactMe: FC<ContactMeInterface> = () => {
+  const form = useRef<HTMLFormElement | null>(null);
+
   const formSubmissionHandler = (e: FormEvent) => {
     e.preventDefault();
-  }
+
+    if (form.current === null) return;
+
+    emailjs
+      .sendForm(
+        'service_qz4ugws',
+        'template_mi83gfy',
+        form.current,
+        'user_HuKpwTBqfsVm40phvlXmn'
+      )
+      .then(
+        (_) => {
+          form.current?.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section className='contact-me column'>
       <h2 className='contact-me__title'>Contact Me</h2>
       <div className='contact-me__content row'>
-        <form className='contact-me__content__form column' onSubmit={formSubmissionHandler}>
+        <form
+          ref={form}
+          className='contact-me__content__form column'
+          onSubmit={formSubmissionHandler}
+        >
           <h3 className='contact-me__content__form__title'> Let's Connect</h3>
-          <input type='text' placeholder='Full Name' required />
-          <input type='email' placeholder='Email' required />
-          <textarea placeholder={`What's up?`} required />
-          <button type='submit'>Send</button>
+          <input
+            type='text'
+            placeholder='Full Name'
+            name='user_name'
+            required
+          />
+          <input type='email' placeholder='Email' name='user_email' required />
+          <textarea placeholder={`What's up?`} name='user_message' required />
+          <button type='submit' value='Send'>
+            Send
+          </button>
         </form>
 
         <div className='contact-me__content__blob column'>
