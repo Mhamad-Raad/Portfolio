4eef58da31c8a0c69e059d43e9adc84daaf0969c
joker69216 <hamaraad883@gmail.com>
2023-04-11 01:06:33 +0300

    add animation on scroll to categories

--- src/Components/Skills/Categories/Categories.tsx
+++ src/Components/Skills/Categories/Categories.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+import { useRef } from 'react';
+import { useInView } from 'framer-motion';
+
 import CategoriesItem from './CategoriesItem';
 
 import './Categories.scss';
@@ -12,9 +15,12 @@ import FlutterImg from '../../../assets/frontend/Flutter.jpg';
 interface CategoriesInterface {}
 
 const Categories: FC<CategoriesInterface> = () => {
+  const catRef = useRef(null);
+  const isInView = useInView(catRef);
   return (
-    <div className='skills-categories row'>
+    <div className='skills-categories row' ref={catRef}>
       <CategoriesItem
+        inView={isInView}
         title='Front-end'
         img={FrontendImg}
         description='Developing responsive pixel perfect web-applications, following best
@@ -22,18 +28,21 @@ const Categories: FC<CategoriesInterface> = () => {
           development approches, such as mobile first development'
       />
       <CategoriesItem
+        inView={isInView}
         title='Student'
         img={StudentImg}
         description='Continues Learner, Constantly trying new technologies out, keeping up
           to date and the new trends'
       />
       <CategoriesItem
+        inView={isInView}
         title='Back-end'
         img={BackendImg}
         description='Developing API, authentication, authorization, using different
           approches such as Test Driven Development'
       />
       <CategoriesItem
+        inView={isInView}
         title='Flutter'
         img={FlutterImg}
         description='Developing responsive, interactive and adaptive Flutter applications'
--- src/Components/Skills/Categories/CategoriesItem.tsx
+++ src/Components/Skills/Categories/CategoriesItem.tsx
@@ -4,15 +4,35 @@ interface CategoriesItemInterface {
   title: string;
   img: string;
   description: string;
+  inView: boolean;
 }
 
 const CategoriesItem: FC<CategoriesItemInterface> = ({
   img,
   title,
   description,
+  inView,
 }) => {
+  let show = '';
+  if (title === 'Front-end') {
+    show = 'translateX(-100%)';
+  } else if (title === 'Student') {
+    show = 'translateY(100%)';
+  } else if (title === 'Back-end') {
+    show = 'translateY(-100%)';
+  } else if (title === 'Flutter') {
+    show = 'translateX(100%)';
+  }
+
   return (
-    <div className='skills-categories__card'>
+    <div
+      className='skills-categories__card'
+      style={{
+        transform: inView ? 'none' : `${ show }`,
+        opacity: inView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+      }}
+    >
       <span className='skills-categories__card__hover-point'></span>
       <span className='skills-categories__card__hover-point'></span>
       <span className='skills-categories__card__hover-point'></span>
