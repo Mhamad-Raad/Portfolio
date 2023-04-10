6af3ef3222072ad57c4ce0e9764f378bb9a9f3fe
joker69216 <hamaraad883@gmail.com>
2023-04-10 22:32:29 +0300

    add tilt effect and adjust structure category item

--- src/Components/Skills/Categories/CategoriesItem.tsx
+++ src/Components/Skills/Categories/CategoriesItem.tsx
@@ -6,20 +6,34 @@ interface CategoriesItemInterface {
   description: string;
 }
 
-const CategoriesItem:FC<CategoriesItemInterface> = ({img, title, description}) => {
+const CategoriesItem: FC<CategoriesItemInterface> = ({
+  img,
+  title,
+  description,
+}) => {
   return (
-    <div className='skills-categories__card column'>
-      <h3 className='skills-categories__card__title'>{title}</h3>
-      <img
-        src={img}
-        alt={title}
-        className='skills-categories__card__img'
-      />
-      <p className='skills-categories__card__description'>
-        {description}
-      </p>
+    <div className='skills-categories__card'>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <span className='skills-categories__card__hover-point'></span>
+      <div className='skills-categories__card__content column'>
+        <h3 className='skills-categories__card__content__title'>{title}</h3>
+        <img
+          src={img}
+          alt={title}
+          className='skills-categories__card__content__img'
+        />
+        <p className='skills-categories__card__content__description'>
+          {description}
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default CategoriesItem;
