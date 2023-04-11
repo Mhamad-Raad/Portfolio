df550d00474866aa7adb67c86e21bb563e3f64fa
joker69216 <hamaraad883@gmail.com>
2023-04-11 23:13:13 +0300

    adjust projects section to typescript

--- src/Components/Projects/Prjs/ProjectList.tsx
+++ src/Components/Projects/Prjs/ProjectList.tsx
@@ -5,17 +5,18 @@ import ProjectItem from './ProjectItem';
 
 import demoImg1 from '../../../assets/ToolsMethodes/GitHub.png';
 
-export default function ProjectList() {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true });
+interface ProjectListInterface {}
+
+const ProjectList: FC<ProjectListInterface> = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const inView: boolean = useInView(ref, { once: true });
 
   return (
     <div
-      className='projects-section__projects row'
+      className={`projects-section__projects row ${
+        inView ? '' : 'projects__framer'
+      }`}
       ref={ref}
-      style={{
-        transform: inView ? 'scale(1)' : 'scale(0)',
-      }}
     >
       <ProjectItem img={demoImg1} />
       <ProjectItem img={demoImg1} />
@@ -24,4 +25,6 @@ export default function ProjectList() {
       <ProjectItem img={demoImg1} />
     </div>
   );
-}
+};
+
+export default ProjectList;
