6bc03a02f397beed4684d4682924bd694036433b
joker69216 <hamaraad883@gmail.com>
2023-04-11 20:15:42 +0300

    add scroll animation

--- src/Components/Projects/Prjs/ProjectList.tsx
+++ src/Components/Projects/Prjs/ProjectList.tsx
@@ -1,10 +1,21 @@
+import { useRef, FC } from 'react';
+import { useInView } from 'framer-motion';
+
 import ProjectItem from './ProjectItem';
 
 import demoImg1 from '../../../assets/ToolsMethodes/GitHub.png';
 
 export default function ProjectList() {
+  const ref = useRef(null);
+  const inView = useInView(ref);
+
   return (
-    <div className='projects-section__projects row'>
+    <div className='projects-section__projects row'
+      ref={ref}
+      style={{
+        transform: inView? 'scale(1)' : 'scale(0)',
+      }}
+    >
       <ProjectItem img={demoImg1} />
       <ProjectItem img={demoImg1} />
       <ProjectItem img={demoImg1} />
