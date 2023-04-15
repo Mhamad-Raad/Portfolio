78a690ccd9dd6698994878682aa4a22fc6e983a0
joker69216 <hamaraad883@gmail.com>
2023-04-15 23:22:40 +0300

    add links to the footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -9,23 +9,27 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = () => {
   return (
     <div className='footer column'>
-      <p className='footer__made-by'>
-        Made with ❤️
-      </p>
+      <p className='footer__made-by'>Made with ❤️</p>
       <div className='footer__social-medias row'>
-        <a href='' className='footer__social-medias__link row'>
+        <a
+          href='https://www.linkedin.com/in/mhamad-raad'
+          className='footer__social-medias__link row'
+        >
           <FaLinkedin />
           <p>Mhamad Radd</p>
         </a>
-        <a href='' className='footer__social-medias__link row'>
+        <a
+          href='https://github.com/Mhamad-Raad'
+          className='footer__social-medias__link row'
+        >
           <FaGithubSquare />
           <p>Mhamad-Radd</p>
         </a>
       </div>
       <p className='footer__copy-rights'>© 2023 - All rights reserved</p>
       <p className='footer__special-thanks'>
-        Special thanks to and everyone else who their
-        tutorials, 3d models and code helped through.
+        Special thanks to and everyone else who their tutorials, 3d models and
+        code helped through.
       </p>
     </div>
   );
