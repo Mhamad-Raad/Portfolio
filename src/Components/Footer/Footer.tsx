a771a9583664fc916e5af8cdf16696a633c8f1f8
joker69216 <hamaraad883@gmail.com>
2023-04-15 22:57:00 +0300

    change fonts in the footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -10,7 +10,6 @@ const Footer: FC<FooterProps> = () => {
       <p className='footer__made-by'>
         Made with ❤️ by <a href='\kjgsrn'>Mhamad Raad</a>
       </p>
-      <p className='footer__copy-rights'>© 2023 - All rights reserved</p>
       <p className='foor__social-medias row'>
         <a href=''>
           <i className='fab fa-facebook-f'></i>
@@ -19,6 +18,7 @@ const Footer: FC<FooterProps> = () => {
           <i className='fab fa-instagram'></i>
         </a>
       </p>
+      <p className='footer__copy-rights'>© 2023 - All rights reserved</p>
       <p className='footer__special-thanks'>
         Special thanks to <a href=''>Dev Ed</a> and everyone else who their
         tutorials, 3d models and code helped through.
