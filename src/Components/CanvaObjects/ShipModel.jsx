207a6ec684a4b6296d73552e886dd489fa853914
joker69216 <hamaraad883@gmail.com>
2023-04-19 18:14:26 +0300

    delete unnecessary code

--- src/Components/CanvaObjects/ShipModel.jsx
+++ src/Components/CanvaObjects/ShipModel.jsx
@@ -1,20 +1,24 @@
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-// import { useGLTF } from '@react-three/drei';
 import Ship from './Ship';
 
-const MovingMeteor = () => {
+const DestinyShip = () => {
   const ref = useRef();
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     ref.current.rotation.y -= delta / 10;
   });
 
   return (
-    <group ref={ref} scale={0.02} position={[-0.8, 0.4, 0]} rotation={[-5.5, 0, 0]}>
+    <group
+      ref={ref}
+      scale={0.02}
+      position={[-0.8, 0.4, 0]}
+      rotation={[-5.5, 0, 0]}
+    >
       <Ship />
     </group>
   );
 };
 
-export default MovingMeteor;
+export default DestinyShip;
