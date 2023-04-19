8d31ae4031f4e86f47678442b0727db005bd324e
joker69216 <hamaraad883@gmail.com>
2023-04-19 22:28:41 +0300

    add responsivity to the moving meteor

--- src/Components/CanvaObjects/MovingMeteor.jsx
+++ src/Components/CanvaObjects/MovingMeteor.jsx
@@ -2,13 +2,21 @@ import { useState, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 
+import useWindowSize from '../../CustomHooks/UseScreenSize';
+
 const MovingMeteor = () => {
+  const { width } = useWindowSize();
   const ref = useRef();
   const [reverse, setReverse] = useState(false);
 
   const { scene } = useGLTF('./models/meteor.glb');
 
-  useFrame((state, delta) => {
+  let scale = 0.1;
+  if (width < 700) {
+    scale = 0.065;
+  }
+
+  useFrame((_, delta) => {
     if (ref.current.position.x < -5 && ref.current.position.y < -5) {
       setReverse(true);
     } else if (ref.current.position.x > 5 && ref.current.position.y > 5) {
@@ -25,7 +33,7 @@ const MovingMeteor = () => {
   });
 
   return (
-    <group position={[-5, -5, 0]} ref={ref} scale={0.1}>
+    <group position={[-5, -5, 0]} ref={ref} scale={scale}>
       <primitive object={scene} />;
     </group>
   );
