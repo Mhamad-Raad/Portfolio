ba98aa743b3640d86ef18f820092bed4a9e59734
joker69216 <hamaraad883@gmail.com>
2023-04-25 19:30:39 +0300

    add data for the projects

---
+++ src/Constants.js
@@ -0,0 +1,37 @@
+export const Projects = [
+  {
+    title: "Project 1",
+    description: "Project 1 description",
+    Languages: ['HTML', 'SCSS', 'React', 'Redux'],
+    link: '',
+    image: '',
+  },
+  {
+    title: "Project 2",
+    description: "Project 2 description",
+    Languages: ['HTML', 'SCSS', 'React', 'Redux'],
+    link: '',
+    image: '',
+  },
+  {
+    title: "Project 3",
+    description: "Project 3 description",
+    Languages: ['HTML', 'SCSS', 'React', 'Redux'],
+    link: '',
+    image: '',
+  },
+  {
+    title: "Project 4",
+    description: "Project 4 description",
+    Languages: ['HTML', 'SCSS', 'React', 'Redux'],
+    link: '',
+    image: '',
+  },
+  {
+    title: "Project 5",
+    description: "Project 5 description",
+    Languages: ['HTML', 'SCSS', 'React', 'Redux'],
+    link: '',
+    image: '',
+  },
+];
