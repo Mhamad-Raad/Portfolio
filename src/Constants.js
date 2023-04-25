ed9c2b2d88d9b47c40096aa08c9a4492cbd2f4d0
joker69216 <hamaraad883@gmail.com>
2023-04-26 00:39:36 +0300

    add other projects

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,9 @@
 import ReserveTravel from './assets/projects/reserve-travel.png';
 import PFM from './assets/projects/PFM.png';
 import Meetups from './assets/projects/Meetups.png';
+import Notes from './assets/projects/Notes.png';
+import Meals from './assets/projects/Meals.png';
+import Cash from './assets/projects/Cash.png';
 
 export const Projects = [
   {
@@ -16,16 +19,16 @@ export const Projects = [
       'Jest',
       'HTML',
       'JavaScript',
-      'npm',
+      'Npm',
     ],
     Llink: 'https://remarkable-buttercream-3be0d2.netlify.app/home',
     GLink: 'https://github.com/Mhamad-Raad/final-capstone-frontend',
     img: ReserveTravel,
   },
   {
-    title: 'Personal Finance Manager',
+    title: 'Finance Manager',
     description:
-      'Introducing Personal Finance Manager - Gateway challenge project! Easily track income, expenses, and save links for the needed finances for later returns. Manage your money like a pro!',
+      'Introducing Finance Manager - Gateway challenge project! Easily track income, expenses, and save links for the needed finances for later returns. Manage your money like a pro!',
     techs: ['Remix', 'SCSS', 'HTML', 'JavaScript', 'Firabase', 'Yarn'],
     Llink: 'https://money-management-jttttj87h-mhamad-raad.vercel.app/',
     GLink: 'https://github.com/Mhamad-Raad/PFM-With-Remix',
@@ -35,25 +38,36 @@ export const Projects = [
     title: 'Meetups',
     description:
       'Meetups lets you see and post events around you. Stay up-to-date with local happenings and share your own events with the community.',
-    techs: ['Next', 'HTML', 'CSS', 'JavaScript', 'MongoDb', 'npm'],
+    techs: ['Next', 'HTML', 'CSS', 'JavaScript', 'MongoDb', 'Npm'],
     Llink: 'https://next-js-news-odq1sgkbk-mhamad-raad.vercel.app/',
     GLink: 'https://github.com/Mhamad-Raad/NextJS-News',
     img: Meetups,
   },
   {
-    title: 'Project 4',
-    description: 'Project 4 description',
-    techs: ['HTML', 'SCSS', 'React', 'Redux'],
-    Llink: '',
-    GLink: '',
-    img: '',
+    title: 'Notes',
+    description:
+      'Looking for a platform to share notes, life advice, or just some good jokes? Look no further! here is the perfect place to share with others. Join us today and start spreading love!',
+    techs: ['Remix', 'HTML', 'CSS', 'JavaScript', 'Firebase', 'Npm'],
+    Llink: 'https://note-collector-jtl0kbpkm-mhamad-raad.vercel.app/Notes',
+    GLink: 'https://github.com/Mhamad-Raad/Remix-Notes',
+    img: Notes,
   },
   {
-    title: 'Project 5',
-    description: 'Project 5 description',
-    techs: ['HTML', 'SCSS', 'React', 'Redux'],
-    Llink: '',
-    GLink: '',
-    img: '',
+    title: 'Meals',
+    description:
+      'Introducing our semi-automated food delivery website! Place your order and our expert cooks will see it from the other side.',
+    techs: ['React', 'Context', 'HTML', 'CSS', 'Javascript', 'Npm'],
+    Llink: 'https://mhamad-raad.github.io/React-Meals/',
+    GLink: 'https://github.com/Mhamad-Raad/React-Meals',
+    img: Meals,
+  },
+  {
+    title: 'Cash Flow',
+    description:
+      "Keep track of your expenses and find out which months you've had the highest spending - and on what. Get a better handle on your finances and start saving today!",
+    techs: ['React', 'HTML', 'CSS', 'Javascript', 'Npm'],
+    Llink: 'https://mhamad-raad.github.io/React-Cash-Flow/',
+    GLink: 'https://github.com/Mhamad-Raad/React-Cash-Flow',
+    img: Cash,
   },
 ];
