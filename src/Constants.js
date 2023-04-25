8bc158bf00c04039c31eafd4ee9b3525ef385452
joker69216 <hamaraad883@gmail.com>
2023-04-25 23:46:49 +0300

    add second project

--- src/Constants.js
+++ src/Constants.js
@@ -1,31 +1,34 @@
 import ReserveTravel from './assets/projects/reserve-travel.png';
+import PFM from './assets/projects/PFM.png';
 
 export const Projects = [
   {
     title: 'Travel Reservation',
     description:
-      'Microverse graduation project, in which users could sign up and users could add/delete travels and users could reserve a travel and see the history of their reservations.',
+      "Microverse graduation project, It's a platform where users can sign up and add or delete their travels. And the best part? You can reserve a travel and even see the history of your past reservations.",
     techs: [
-      'HTML',
-      'CSS',
       'React',
       'Redux',
       'Ruby on Rails',
       'Rspec',
       'JWT',
       'Jest',
+      'HTML',
+      'JavaScript',
+      'npm',
     ],
     Llink: 'https://remarkable-buttercream-3be0d2.netlify.app/home',
     GLink: 'https://github.com/Mhamad-Raad/final-capstone-frontend',
     img: ReserveTravel,
   },
   {
-    title: 'Project 2',
-    description: 'Project 2 description',
-    techs: ['HTML', 'SCSS', 'React', 'Redux'],
-    Llink: '',
-    GLink: '',
-    img: '',
+    title: 'Personal Finance Manager',
+    description:
+      'Gateway challenge project, Introducing Personal Finance Manager! A simple application designed to help you track your income and expenses while managing your money like a pro, save filtered links for later returns ;).',
+    techs: ['Remix', 'SCSS', 'HTML', 'JavaScript', 'Yarn'],
+    Llink: 'https://money-management-jttttj87h-mhamad-raad.vercel.app/',
+    GLink: 'https://github.com/Mhamad-Raad/PFM-With-Remix',
+    img: PFM,
   },
   {
     title: 'Project 3',
