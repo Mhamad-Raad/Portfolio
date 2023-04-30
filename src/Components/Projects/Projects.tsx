03a2da55bc156f83d003fb7918f5293ad50dfb3e
joker69216 <hamaraad883@gmail.com>
2023-04-30 15:12:06 +0300

    add filter bar

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,16 +1,46 @@
 import { FC } from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
 import ProjectList from './Prjs/ProjectList';
-
 
 import './Projects.scss';
 
 interface ProjectsInterface {}
 
+const animatedComponents = makeAnimated();
+
 const Projects: FC<ProjectsInterface> = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
     <section className='projects-section column' id='projects'>
-      <h2 className='projects-section__title'>Projects</h2>
+      <div className='projects-section__header row'>
+        <h2 className='projects-section__header__title'>Projects</h2>
+        <Select
+          options={options}
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          isMulti
+          className='projects-section__header__select'
+          styles={{
+            multiValue: (styles) => {
+              return {
+                ...styles,
+                backgroundColor: '#193155',
+              };
+            },
+            multiValueLabel: (styles) => ({
+              ...styles,
+              color: '#8ab6f9',
+            }),
+          }}
+        />
+      </div>
       <ProjectList />
     </section>
   );
