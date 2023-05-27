4bb4325b347a14d71dafcc381881af88abfb4003
joker69216 <hamaraad883@gmail.com>
2023-05-27 19:23:15 +0300

    add structure for the contact me description

--- src/Components/ContactMe/ContactMe.tsx
+++ src/Components/ContactMe/ContactMe.tsx
@@ -11,6 +11,12 @@ const ContactMe: FC<ContactMeInterface> = () => {
   return (
     <section className='contact-me column' id='contact'>
       <h2 className='contact-me__title'>Contact Me</h2>
+      <p className='contact-me__descriotion'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni impedit
+        vel doloremque, adipisci est velit quasi, voluptates, suscipit odio
+        consequatur officia eligendi sed consectetur tempora? Voluptatibus
+        tempora incidunt nesciunt saepe.
+      </p>
       <div className='contact-me__content row'>
         <ContactMeForm />
         <div className='contact-me__content__blob column'>
